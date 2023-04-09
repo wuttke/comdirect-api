49da37ab0234ca8205f2d66b2ca8a97420209c0d
Matthias Wuttke <wuttke@meona.de>
2023-04-09 22:26:58 +0200

    test more fields

--- src/test/java/eu/wuttke/comdirect/account/AccountServiceTest.java
+++ src/test/java/eu/wuttke/comdirect/account/AccountServiceTest.java
@@ -65,12 +65,31 @@ class AccountServiceTest {
                 .thenReturn(new SimpleHttpResponse(transactionsTestData, 200, new HashMap<>()));
         var page = service.getTransactions(session, "Account123");
         assertEquals(1, page.getValues().size());
+
         assertEquals("Accept", headers.getValue()[0]);
         assertEquals("application/json", headers.getValue()[1]);
         assertEquals("Authorization", headers.getValue()[2]);
         assertEquals("Bearer " + session.getTokens().getAccessToken(), headers.getValue()[3]);
         assertEquals("x-http-request-info", headers.getValue()[4]);
-        assertEquals("6V2C1LVP2LJQ958S/22169", page.getValues().get(0).getReference());
+
+        Transaction t1 = page.getValues().get(0);
+        assertEquals("6V2C1LVP2LJQ958S/22169", t1.getReference());
+
+        assertEquals("BOOKED", t1.getBookingStatus());
+        assertEquals("2023-04-05", t1.getBookingDate());
+        assertEquals(-22.0, t1.getAmount().getValue());
+        assertEquals("EUR", t1.getAmount().getUnit());
+        assertEquals("GASTST PARADIES", t1.getRemitter().getHolderName());
+        assertEquals(null, t1.getDeptor());
+        assertEquals(null, t1.getCreditor());
+        assertEquals("2023-04-05", t1.getValutaDate());
+        assertEquals(null, t1.getDirectDebitCreditorId());
+        assertEquals(null, t1.getDirectDebitMandateId());
+        assertEquals(null, t1.getEndToEndReference());
+        assertEquals(true, t1.isNewTransaction());
+        assertEquals("01GASTSTAETTE PARADIES//FREIBURG/DE  022023-04-04T13:46:59 KFN 1  VJ 2312 ", t1.getRemittanceInfo());
+        assertEquals("Direct Debit", t1.getTransactionType().getText());
+        assertEquals("DIRECT_DEBIT", t1.getTransactionType().getKey());
     }
 
     private static String accountsTestData = "{\"paging\":{\"index\":0,\"matches\":3},\"values\":[{\"account\":{\"accountId\":\"64F12122866E43D58984BD2CE60FB84D\",\"accountDisplayId\":\"102327881900\",\"currency\":\"EUR\",\"clientId\":\"5CBCEE21441E4669B6E0C162C95737BE\",\"accountType\":{\"key\":\"CA\",\"text\":\"Girokonto\"},\"iban\":\"DE14200411330327881900\",\"bic\":\"COBADEHD001\",\"creditLimit\":{\"value\":123.0,\"unit\":\"EUR\"}},\"accountId\":\"64F12122866E43D58984BD2CE60FB84D\",\"balance\":{\"value\":123.45,\"unit\":\"EUR\"},\"balanceEUR\":{\"value\":123.45,\"unit\":\"EUR\"},\"availableCashAmount\":{\"value\":123.45,\"unit\":\"EUR\"},\"availableCashAmountEUR\":{\"value\":123.45,\"unit\":\"EUR\"}},{\"account\":{\"accountId\":\"B04B6144107E4D59A431C506A684F6EA\",\"accountDisplayId\":\"102327881905\",\"currency\":\"EUR\",\"clientId\":\"5CBCEE21441E4669B6E0C162C95737BE\",\"accountType\":{\"key\":\"DAS\",\"text\":\"Tagesgeld PLUS-Konto\"},\"iban\":\"DE73200411330327881905\",\"bic\":\"COBADEHD001\",\"creditLimit\":{\"value\":0.0,\"unit\":\"EUR\"}},\"accountId\":\"B04B6144107E4D59A431C506A684F6EA\",\"balance\":{\"value\":0.0,\"unit\":\"EUR\"},\"balanceEUR\":{\"value\":0.0,\"unit\":\"EUR\"},\"availableCashAmount\":{\"value\":0.0,\"unit\":\"EUR\"},\"availableCashAmountEUR\":{\"value\":0.0,\"unit\":\"EUR\"}},{\"account\":{\"accountId\":\"EB212CD8BC20467B8C9F4764D8B577C6\",\"accountDisplayId\":\"102327881980\",\"currency\":\"USD\",\"clientId\":\"5CBCEE21441E4669B6E0C162C95737BE\",\"accountType\":{\"key\":\"FX\",\"text\":\"Fremdwährungskonto\"},\"iban\":\"DE85200411330327881980\",\"bic\":\"COBADEHD001\",\"creditLimit\":{\"value\":0.0,\"unit\":\"EUR\"}},\"accountId\":\"EB212CD8BC20467B8C9F4764D8B577C6\",\"balance\":{\"value\":0.0,\"unit\":\"USD\"},\"balanceEUR\":{\"value\":0.0,\"unit\":\"EUR\"},\"availableCashAmount\":{\"value\":0.0,\"unit\":\"USD\"},\"availableCashAmountEUR\":{\"value\":0.0,\"unit\":\"EUR\"}}]}\n";
